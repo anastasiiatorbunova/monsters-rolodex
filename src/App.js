94daf37ec0b2054450111bb7833709545686aec0
anastasiiatorbunova <anastasiiatorbunova@gmail.com>
2022-06-25 18:12:47 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => 
+      monster.name.toLocaleLowerCase().includes(searchField)
+    );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className='app'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+
+      <SearchBox 
+        onChangeHandler={onSearchChange} 
+        placeholder='Search monsters' 
+        className='monsters-search-box' 
+      />
+      <CardList monsters = {filteredMonsters} />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/card-list/card/card.component.jsx
@@ -0,0 +1,17 @@
+const Card = ({monster}) => {
+    const {id, name, email} = monster;
+
+    return (
+        <div key={id} className='card-container'>
+            <img 
+                alt={`Monster ${name}`} 
+                src={`https://robohash.org/${id}?set=set2&size=180x180`} 
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+};
+ 
+
+export default Card;
